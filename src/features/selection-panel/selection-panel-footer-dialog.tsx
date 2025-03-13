1eee917948438c2ccdd14a9fb61ddd835afee15a
Ji Hae Kim (ji-kim) <53713342+deftones88@users.noreply.github.com>
2025-03-13 17:30:18 +0900

    fijx: dialog description error

--- src/features/selection-panel/selection-panel-footer-dialog.tsx
+++ src/features/selection-panel/selection-panel-footer-dialog.tsx
@@ -18,33 +18,35 @@ export const SelectionPanelFooterDialog = () => {
       <DialogContent>
         <DialogHeader>
           <DialogTitle>버전 히스토리</DialogTitle>
-          <DialogDescription>
-            {VERSION_HISTORY.map((versionGroup) => (
-              <div key={versionGroup.majorVersion}>
-                {versionGroup.subVersions.map((subVersion) => (
-                  <div key={subVersion.version}>
-                    <h6 className="pt-2 font-bold">
-                      v{versionGroup.majorVersion}
-                    </h6>
-                    {subVersion.updates.map((minorVersion) => (
-                      <p key={minorVersion.version} className="ml-2">
-                        <span className="inline-block w-11">
-                          v{versionGroup.majorVersion}.{subVersion.version}.
-                          {minorVersion.version}
-                        </span>
-                        <span>: {minorVersion.update}</span>
-                      </p>
-                    ))}
-                  </div>
-                ))}
-              </div>
-            ))}
-            <h6 className="pt-8 pb-2 font-bold"> 업데이트 계획</h6>
-            <ul className="ml-6 list-disc [&>li]:mt-1">
-              {TODOS.map((todo) => (
-                <li key={todo}>{todo}</li>
+          <DialogDescription asChild>
+            <div>
+              {VERSION_HISTORY.map((versionGroup) => (
+                <div key={versionGroup.majorVersion}>
+                  <h6 className="pt-2 font-bold">
+                    v{versionGroup.majorVersion}
+                  </h6>
+                  {versionGroup.subVersions.map((subVersion) => (
+                    <div key={subVersion.version}>
+                      {subVersion.updates.map((minorVersion) => (
+                        <div key={minorVersion.version} className="ml-2">
+                          <span className="inline-block w-11">
+                            v{versionGroup.majorVersion}.{subVersion.version}.
+                            {minorVersion.version}
+                          </span>
+                          <span>: {minorVersion.update}</span>
+                        </div>
+                      ))}
+                    </div>
+                  ))}
+                </div>
               ))}
-            </ul>
+              <h6 className="pt-8 pb-2 font-bold"> 업데이트 계획</h6>
+              <ul className="ml-6 list-disc [&>li]:mt-1">
+                {TODOS.map((todo) => (
+                  <li key={todo}>{todo}</li>
+                ))}
+              </ul>
+            </div>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
